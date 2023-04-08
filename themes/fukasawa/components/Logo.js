d9fbc5f97235f525a58cfd7de68510183fd4db56
stan7s <stan7@qq.com>
2023-04-07 21:17:21 -0400

    Remove title hover effect

--- themes/fukasawa/components/Logo.js
+++ themes/fukasawa/components/Logo.js
@@ -6,7 +6,7 @@ const Logo = props => {
    <section className='flex'>
     <Link
      href='/'
-     className='hover:bg-black hover:text-white duration-500 px-4 py-2 cursor-pointer dark:text-gray-300 dark:border-gray-300 font-black'>
+     className='px-2 py-2 cursor-pointer dark:text-gray-300 dark:border-gray-300 font-black'>
      {siteInfo?.title}
     </Link>
    </section>
